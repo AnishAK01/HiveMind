d76d4251dcda1d98edb6d33c8c7830509c48b306
Anish Kumar <112410410+AnishAK01@users.noreply.github.com>
2025-07-17 02:27:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(cors({
-  origin: "http://localhost:5173"|| "https://hivemind-frontend.onrender.com", 
+  origin:  "https://hivemind-frontend.onrender.com", 
   credentials: true
 }));
 app.use(express.json()); // Body parser
