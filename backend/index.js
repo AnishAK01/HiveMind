11b68548c35865d61d868648e2c5fb248f5e6165
Anish Kumar <112410410+AnishAK01@users.noreply.github.com>
2025-07-17 02:04:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,25 +9,25 @@ const userRoutes = require('./routes/user.routes');
 const path = require('path');
 
 dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(express.json()); // Body parser
-
-connectDB(); // Connect to MongoDB
-
-// Basic route
-app.get('/', (req, res) => {
-  res.send("Welcome to Hivemind API");
-});
 app.use(cors({
   origin: "https://hivemind-frontend.onrender.com", 
   credentials: true
 }));
+const app = express();
+app.use(cors());
+app.use(express.json()); 
+
+connectDB();
+
+
+app.get('/', (req, res) => {
+  res.send("Welcome to Hivemind API");
+});
+
 
 app.use('/api/users', require('./routes/auth.routes'));
 app.use('/api/posts', require('./routes/post.routes'));
-app.use('/api/users', require('./routes/user.routes'));
+// app.use('/api/users', require('./routes/user.routes'));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
